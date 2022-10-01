59b378c6310d722c19e9f1b2b683615cf5a23aca
Yoi-Nami-Ra <yoinamira@gmail.com>
2022-10-01 21:56:46 +0200

    Empty app draft

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Jarosław Wojtasik on 01/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
